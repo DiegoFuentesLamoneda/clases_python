c2f53608d6d983789bda3a16a817b88039ee69e9
DiegoFuentesLamoneda <illice21games@gmail.com>
2023-03-14 09:41:57 +0100

    Fichero test

---
+++ test.py
@@ -0,0 +1,18 @@
+a = 1
+b = 3.5
+c = "perro"
+d = [1, 2, 3, 4, 5, 6, "casa", 21.9]
+e = {"a": 1, "b": 2, "c": 4.5, "d": "z"}
+
+print("El valor de a es: type(a)")
+print(f"El tipo de dato para la variable a es: {type(a)}")
+print(f"El tipo de dato para la variable c es: {type(c)}")
+print(f"El tipo de dato para la variable e es: {type(e)}")
+print(f"El tipo de dato para la variable d es: {type(d)}")
+print(f"El tipo de dato para la variable b es: {type(b)}")
+numero1 = int(input("Dame un número "))
+numero2 = int(input("Dame un número "))
+print(numero1+numero2)
+numero3 = input("Dame un número ")
+numero4 = input("Dame un número ")
+print(numero3+numero4)
